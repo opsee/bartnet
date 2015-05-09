adc0c5c33b1d079a2d542feb20876415155a1ca9
Greg Poirier <grep@binary-snobbery.com>
2015-05-08 18:16:07 -0700

    Dockerize bartnet

---
+++ tasks/leiningen/docker.clj
@@ -0,0 +1,15 @@
+(ns leiningen.docker
+  (:require [leiningen.jar :refer [get-jar-filename]]
+            [leiningen.uberjar :refer [uberjar]]
+            [clojure.java.shell :refer [sh]]
+            [clojure.string :refer [blank?]]))
+
+(defn docker
+  "Build docker image"
+  [project]
+  (uberjar project)
+  (let [jar-path (get-jar-filename project :standalone)]
+    (sh "cp" jar-path "docker/lib/bartnet.jar")
+    (let [docker (sh "docker" "build" "-t" "bartnet" "docker")]
+      (println (:out docker))
+      (if-not (blank? (:err docker)) (println "Build error: " (:err docker))))))
