3900418c73c0d1d6defbbc44527604cb8254271d
Cliff Moon <cliff@leaninto.it>
2015-08-19 15:43:16 -0700

    change page logic for clarity, fix tests

--- src/bartnet/api.clj
+++ src/bartnet/api.clj
@@ -465,9 +465,9 @@
       (sql/insert-into-signups! @db signup))))
 
 (defn- page-count [count query-limit]
-  (if (< 0 (rem count query-limit))
-    (+ 1 (quot count query-limit))
-    (quot count query-limit)))
+  (if (= 0 (rem count query-limit))
+    (quot count query-limit)
+    (+ 1 (quot count query-limit))))
 
 (defn list-signups [ctx]
   (let [page (or (get-in ctx [:request :params "page"]) 1)
