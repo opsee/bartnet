7c3e39b4e89179fd80654918c17be748864fadb6
Cliff Moon <cliff@leaninto.it>
2015-09-06 11:47:16 -0700

    implement an nsqlookup strategy that can survive empty lookups

--- src/bartnet/nsq.clj
+++ src/bartnet/nsq.clj
@@ -3,11 +3,23 @@
             [bartnet.identifiers :as identifiers]
             [bartnet.bus :as bus])
   (:import (com.github.brainlag.nsq.lookup DefaultNSQLookup)
-           (com.github.brainlag.nsq NSQProducer NSQConsumer)
+           (com.github.brainlag.nsq NSQProducer NSQConsumer ServerAddress)
            (bartnet.bus InternalBus Consumer)
            (com.github.brainlag.nsq.callbacks NSQMessageCallback)
-           (org.apache.http.impl.client HttpClients CloseableHttpClient)
-           (org.apache.http.client.methods HttpGet)))
+           (com.google.common.collect Sets)
+           (java.io IOException)))
+
+(defn nsq-lookup [lookup-addr produce-addr]
+  (let [proxy (proxy [DefaultNSQLookup] []
+                (lookup [topic]
+                  (try
+                    (proxy-super lookup topic)
+                    (catch IOException _
+                      (let [set (Sets/newHashSet)]
+                        (.add set (ServerAddress. (:host produce-addr) (:port produce-addr)))
+                        set)))))]
+    (.addLookupAddress proxy (:host lookup-addr) (:port lookup-addr))
+    proxy))
 
 (defn delivery [client topic]
   (proxy [NSQMessageCallback] []
@@ -15,34 +27,10 @@
       (bus/deliver-to client topic (parse-string (String. (.getMessage msg)) true))
       (.finished msg))))
 
-(defn- create-topic [^CloseableHttpClient http-client host port topic]
-  )
-
-(defn- does-topic-exist [^CloseableHttpClient http-client host port topic]
-  (let [http-get (HttpGet. (str "http://" host ":" port "/lookup?topic=" topic))
-        response (.execute http-client http-get)]
-    (try
-      (let [status (-> response
-                       .getStatusLine
-                       .getStatusCode)]
-        (and (> status 199)
-             (< status 300)))
-      (finally (.close response)))))
-
-(defn- create-topic-if-necessary [^CloseableHttpClient http-client config topic]
-  (when-not (does-topic-exist http-client
-                              (get-in config [:lookup :host])
-                              (get-in config [:lookup :port]) topic)
-    (create-topic http-client
-                  (get-in config [:producer :host])
-                  (get-in config [:producer :host]) topic)))
-
-
 (defn message-bus [config]
   (let [lookup-addr (:lookup config)
         produce-addr (:producer config)
-        http-client (HttpClients/createDefault)
-        lookup (doto (DefaultNSQLookup.) (.addLookupAddress (:host lookup-addr) (:port lookup-addr)))
+        lookup (nsq-lookup lookup-addr produce-addr)
         producer (-> (NSQProducer.)
                      (.addAddress (:host produce-addr) (:port produce-addr))
                      (.start))]
