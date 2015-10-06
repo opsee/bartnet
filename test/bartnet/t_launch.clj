baab00449cdc6f3b975763e01e83802a492a3fe3
Mark Martin <assplecake@gmail.com>
2015-10-06 11:57:37 -0700

    i guess you gonna do what u want

--- test/bartnet/t_launch.clj
+++ test/bartnet/t_launch.clj
@@ -16,9 +16,7 @@
        "    BASTION_ID=dorpydorp\n"
        "    VPN_PASSWORD=doopydoop\n"
        "coreos:\n"
-       "  update:\n"
-       "    reboot_strategy: etcd-lock\n"
-       "    group: alpha\n"))
+       "  update: {reboot-strategy: etcd-lock, group: alpha}\n"))
 
 (with-fake-routes {"https://vape.opsy.co/bastions" {:post (fn [request] {:status 200 :headers {} :body "{\"id\":\"dorpydorp\",\"password\":\"doopydoop\"}"})}}
                   ;; Exact string match:
