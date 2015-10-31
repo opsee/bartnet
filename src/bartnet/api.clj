1c84b55abaa0e2a4ca8be196302b5274ffe8d2a0
Mark Martin <assplecake@gmail.com>
2015-10-31 12:49:33 -0700

    let me have dis swagger

--- src/bartnet/api.clj
+++ src/bartnet/api.clj
@@ -405,17 +405,14 @@
 
     (GET* "/" []
       :summary "Retrieve a list of groups."
-      :no-doc true
       (groups-resource nil))
 
     (GET* "/:type" [type]
       :summary "Retrieve a list of security groups."
-      :no-doc true
       (groups-resource {:type type}))
 
     (GET* "/:type/:id" [type id]
       :summary "Retrieve a list of instances belonging to a security group."
-      :no-doc true
       (groups-resource {:id id :type type})))
 
   (GET* "/group/:type/:id" [type id]
@@ -423,7 +420,6 @@
 
   (GET* "/customer" []
     :summary "Retrieve a customer from the instance store."
-    :no-doc true
     (customers-resource))
 
   (rt/not-found "Not found."))
