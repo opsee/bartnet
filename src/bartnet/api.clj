4af0c9f745120f3752a05a1350dc50dc198a0caf
Mark Martin <assplecake@gmail.com>
2016-04-19 15:10:56 -0700

    do this instead

--- src/bartnet/api.clj
+++ src/bartnet/api.clj
@@ -212,7 +212,7 @@
       (sql/insert-into-checks! @db (assoc db-check :customer_id customer-id))
       (all-bastions (:customer_id login) #(rpc/create-check % checks))
       (log/debug "check" ided-check)
-      {:checks [ided-check]})))
+      {:checks-resource {:checks [ided-check]}})))
 
 
 (defn reboot-instances! [^RebootInstancesRequest rebootInstancesRequest]
@@ -388,7 +388,7 @@
   :allowed-methods [:get :post]
   :authorized? (authorized?)
   :post! (create-check! checks)
-  :handle-created (fn [ctx] ctx)
+  :handle-created :checks-resource
   :handle-ok list-checks)
 
 (defresource gql-checks-resource [checks]
