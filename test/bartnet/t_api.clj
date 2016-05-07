e501c03f9c9c335feda0b1e632b0ed366f584e4b
Michael Borsuk <mrb@wiredsoul.org>
2016-05-06 17:38:27 -0700

    migrate tables and query for new target pkeys
    - target id changes for composite keys
    - pass custid to target resolve

--- test/bartnet/t_api.clj
+++ test/bartnet/t_api.clj
@@ -126,6 +126,7 @@
                                                                              :name "A Good Check"
                                                                              :target {:name "goobernetty"
                                                                                       :type "sg"
+                                                                                      :customer_id "154ba57a-5188-11e5-8067-9b5f2d96dce1"
                                                                                       :id "sg123679"}
                                                                              :check_spec {:type_url "HttpCheck"
                                                                                           :value {:name "A Good Check"
@@ -187,6 +188,7 @@
                                                                                   :name "doop"
                                                                                   :target {:name "goobernetty"
                                                                                            :type "sg"
+                                                                                           :customer_id "154ba57a-5188-11e5-8067-9b5f2d96dce1"
                                                                                            :id "sg123"}
                                                                                   :check_spec {:type_url "HttpCheck"
                                                                                                :value {:name "doop"
@@ -210,6 +212,7 @@
                                                                            :name "A Good Check"
                                                                            :target {:name "goobernetty"
                                                                                     :type "sg"
+                                                                                    :customer_id "154ba57a-5188-11e5-8067-9b5f2d96dce1"
                                                                                     :id "sg123"}
                                                                            :check_spec {:type_url "HttpCheck"
                                                                                         :value {:name "A Good Check"
